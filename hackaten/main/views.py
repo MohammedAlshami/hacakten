698a22df3ed3b8a30531773d622a53314901bd1b
MohammedAlshami <137855918+MohammedShami@users.noreply.github.com>
2023-10-22 18:41:22 +0800

    modified filename

--- hackaten/main/views.py
+++ hackaten/main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'landing\index.html')
+    return render(request, 'landing/index.html')
